c1416a6561f468478f89360af1d80cb97bc8ae1b
TylerB2402 <77219984+TylerB2402@users.noreply.github.com>
2021-01-10 19:39:04 +0000

    !pnl update

--- main.py
+++ main.py
@@ -82,8 +82,25 @@ def get_percentage_change_day():
   percday = json_data['result']['changeBod'] # alter string to retrieve what I need
   return(percday)
 
+headers3 = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0',
+    'Accept': '*/*',
+    'Accept-Language': 'en-GB,en;q=0.5',
+    'ftx-client': 'web',
+    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJ1c2VyfHR5bGVybGVlYmVkZG93QGhvdG1haWwuY28udWsiLCJpc3MiOiJmdHguY29tIiwibmJmIjoxNjEwMjI5OTI5LCJleHAiOjE2MTI4MjE5ODksImF1ZCI6Imh0dHBzOi8vZnRleGNoYW5nZS5jb20vYXBpLyIsImlhdCI6MTYxMDIyOTk4OSwibWZhIjp0cnVlLCJ3aXRoZHJhd2Fsc0Rpc2FibGVkIjpmYWxzZSwiaW50ZXJuYWxUcmFuc2ZlcnNEaXNhYmxlZCI6ZmFsc2UsInJlYWRPbmx5IjpudWxsfQ.dz8X8TCHjUR2lCjcW4GRwLGyfPgUABBolEXrrYyjQzn7xdxrhSUK906UxDuQa-hvYo_JDLCxhwnwr4yTqGk1j3-v6LrY6WQgb-10HWrIPijrG9-0fqq36z2Mj1LRKqpPW06QfhL8To2_ONqKcl4ZoEV-uitZrUcigNjj_EDECA6M_CO6LiJAbej7H-nAKx_D5CeOTJt1rqCK3lBccirqTVqxWQYQqaeKOAbl1w2OayxKtxM3wZ6TbrLHQZOjGPx5NJKUTjz10Jy69sOx82sn08juLppbDoV1vbrXUiVqe5XCWcaQbT11984TzwNvu66nAneIO55ceVQNedh5i_qBNA',
+    'Referer': 'https://ftx.com/trade/BTC-PERP',
+    'Connection': 'keep-alive',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
+    'TE': 'Trailers',
+}
+
+params3 = (
+    ('showAvgPrice', 'true'),
+)
+
 def get_current_pnl():
-  response = requests.get('https://ftx.com/api/positions')
+  response = requests.get('https://ftx.com/api/positions', headers=headers3, params=params3)
   json_data = json.loads(response.text) # parse response into json format/string
   currentpnl = json_data['result'][0]['recentPnl'] # alter string to retrieve what I need
   return(currentpnl)
